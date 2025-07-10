f8c9d8358e4dad79d3d515f00d23791bd4b63537
DouglasTeixeiraLima1 <douglastlima17@outlook.com>
2025-07-10 11:16:02 -0300

    Reconhecer com ou sem acentuação

--- index.py
+++ index.py
@@ -1,32 +1,44 @@
 from deep_translator import GoogleTranslator 
+from unidecode import unidecode
 
 #Texto para ser traduzido
 texto = input("Mensagem:\n") 
 
+#Dicionário de idiomas(Dict)
 mapa_idiomas = {
     "russo": "ru",
-    "português-br": "pt",
-    "inglês": "en",
+    "portugues-br": "pt",
+    "ingles": "en",
     "espanhol": "es",
     "italiano": "it",
-    "alemão": "de",
-    "francês": "fr"
+    "alemao": "de",
+    "frances": "fr",
+    "japones": "ja",
+    "arabe": "ar",
+    "chines":"zh",
+    "grego":"el",
+    "islandes":"is",
+    "hebraico":"iw",
+    "coreano":"ko",
+    "noruegues":"no",
+    "ucraniano":"uk"
 }
 
 escolha = True
 while escolha:
-    # Pegando o input do usuário
     nome = input("Digite o idioma que você quer ler:\n").lower()
-# Verificando se está no dicionário
-    if nome in mapa_idiomas:
-        target = mapa_idiomas[nome]
+    nome_sem_acento = unidecode(nome)
+    if nome_sem_acento in mapa_idiomas:
+        target = mapa_idiomas[nome_sem_acento] 
         escolha = False
     else:
-        print("Nome não encontrado, tente novamente.")
+        print("Idioma não encontrado! Tente novamente.")
+        #teste 
+        print(nome_sem_acento)
 
-
-#I.A identificar idioma de origem do texto e traduzir para o idioma desejado
+#identifica idioma de origem do texto e traduz para o idioma desejado
 tradutor = GoogleTranslator(Source ="auto", target= target)
 
+#Variável de suporte com o parâmetro para a tradução
 traducao = tradutor.translate(texto)
 print(f"Mensagem:\n{traducao}")
